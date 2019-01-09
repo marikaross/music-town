2d848a976280f11786c0ac38c91139f7b0c53e85
Marika Ross <marika.ross@gmail.com>
2019-01-09 16:19:41 -0700

    Add isAddButtonDisabled function to controller and passed to template

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,4 +1,11 @@
+import { isEmpty } from '@ember/utils';
+import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+  name: '',
+
+  isAddButtonDisabled: computed('name', function() {
+    return isEmpty(this.get('name'))
+  })
 });
