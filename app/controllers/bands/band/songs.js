0b969bae68c4c2ad15f4cbb481b689db9afe4f3d
Marika Ross <marika.ross@gmail.com>
2019-01-10 07:13:20 -0700

    Direct user to create first song

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -5,10 +5,14 @@ import Controller from '@ember/controller';
 export default Controller.extend({
   title: '',
 
+  noSongs: computed('model.songs.[]', function() {
+    return this.get('model.songs.length') === 0
+  }),
+
   isAddButtonDisabled: computed('title', function() {
       return isEmpty(this.get('title'));
   }), 
-  
+
   actions: {
     updateRating: function(params) {
       var song = params.item,
