0d5622045d61201b66cc1595af9347b3bef6b5d5
Marika Ross <marika.ross@gmail.com>
2019-01-10 06:57:38 -0700

    Disable buttons when no input

--- app/controllers/songs.js
+++ app/controllers/songs.js
@@ -1,5 +1,6 @@
 import { isEmpty } from '@ember/utils';
 import { computed } from '@ember/object';
+
 import Controller from '@ember/controller';
 
 export default Controller.extend({
@@ -7,14 +8,5 @@ export default Controller.extend({
 
   isAddButtonDisabled: computed('title', function() {
       return isEmpty(this.get('title'));
-  }), 
-  
-  actions: {
-    updateRating: function(params) {
-      var song = params.item,
-      rating = params.rating;
-
-      song.set('rating', rating);
-    }
-  }
+  })
 });
